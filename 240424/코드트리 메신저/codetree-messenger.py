9465a848dbca1fa504fb62fe324f5048de9fa6ee
이상협 <75459370+723poil@users.noreply.github.com>
2024-04-24 20:06:01 +0900

    Title: 코드트리 메신저, Time: 8022ms, Memory: 48MB, Status: Time Limit Exceed - CodeTree

--- 240424/코드트리 메신저/codetree-messenger.py
+++ 240424/코드트리 메신저/codetree-messenger.py
@@ -5,14 +5,18 @@ input = stdin.readline
 def can_alarms(node: int, depth: int = 1)-> int:
     count = 0
 
+    if not tree_dict.get(node):
+        return count
+
     # 1. 자식노드 부터 시작(depth + 1)
     # 2. setup설정이 꺼져있으면 넘기기
     # 3. 세기를 확인하고 가능하면 + 1
-    for i, p in enumerate(tree):
-        if p == node and alarms[i]:
-            count += can_alarms(i+1, depth + 1)
+    for child in tree_dict[node]:
+        if alarms[child-1]:
+            count += can_alarms(child, depth + 1)
 
-            if authorities[i] >= depth:
+            if authorities[child-1] >= depth:
+                # print(node, child, alarms[child-1])
                 count += 1
 
     return count
@@ -25,6 +29,8 @@ if __name__ == '__main__':
     authorities = []
     alarms = [True for _ in range(N)]
 
+    tree_dict = dict()
+
     for q in range(Q):
         commands = list(map(int, input().split()))
 
@@ -33,6 +39,12 @@ if __name__ == '__main__':
         if command == 100:
             tree = commands[1:N+1]
             authorities = commands[N+1:]
+
+            for i, p in enumerate(tree):
+                if not tree_dict.get(p):
+                    tree_dict[p] = [i+1]
+                else:
+                    tree_dict[p].append(i+1)
         
         elif command == 200:
             alarms[commands[1]-1] = not alarms[commands[1]-1]
@@ -44,9 +56,24 @@ if __name__ == '__main__':
             a = commands[1]
             b = commands[2]
 
+            a_index = tree_dict[tree[a-1]].index(a)
+            b_index = tree_dict[tree[b-1]].index(b)
+
+            # print(11111, tree_dict[tree[a-1]], tree_dict[tree[b-1]])
+            # print(11111, alarms[a-1], alarms[b-1])
+
             temp = tree[a-1]
             tree[a-1] = tree[b-1]
             tree[b-1] = temp
+
+            a_index = tree_dict[tree[b-1]].index(a)
+            b_index = tree_dict[tree[a-1]].index(b)
+            
+            tree_dict[tree[a-1]][b_index] = a
+            tree_dict[tree[b-1]][a_index] = b
+
+            # print(11111, a, tree_dict[tree[a-1]], tree_dict[tree[b-1]])
+            # print(11111, a, alarms[a-1], alarms[b-1])
 
         elif command == 500:
             print(can_alarms(commands[1]))
