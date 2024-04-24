63c6a9515422fb6abdcf49b194c1696c9f4d4b0d
이상협 <75459370+723poil@users.noreply.github.com>
2024-04-24 23:08:00 +0900

    Title: 코드트리 메신저, Time: 1502ms, Memory: 68MB, Status: Passed - CodeTree

--- 240424/코드트리 메신저/codetree-messenger.py
+++ 240424/코드트리 메신저/codetree-messenger.py
@@ -3,46 +3,120 @@ from collections import deque
 
 input = stdin.readline
 
-def can_alarms(node: int)-> int:
-    count = 0
+def setup(commands: list):
+    for i in range(1, N + 1):
+        parents[i] = commands[i]
 
-    q = deque()
-    q.append([node, 1])
+    for i in range(1, N + 1):
+        authorities[i] = commands[i + N]
 
-    while q:
-        n, depth = q.popleft()
+        if authorities[i] > 20:
+            authorities[i] = 20
 
-        if not tree_dict.get(n):
-            continue
+    for i in range(1, N + 1):
+        cur_node = i
+        can_depth = authorities[i]
+        tree[cur_node][can_depth] += 1
 
-        for child in tree_dict[n]:
-            if alarms[child-1]:
-                q.append([child, depth + 1])
+        while parents[cur_node] and can_depth:
+            cur_node = parents[cur_node]
+            can_depth -= 1
 
-                if authorities[child - 1] >= depth:
-                    count += 1
+            if can_depth:
+                tree[cur_node][can_depth] += 1
+            
+            able_counts[cur_node] += 1
+
+
+def set_alarm(node: int):
+    cur_node = parents[node]
+    num = 1
+
+    while cur_node:
+        for depth in range(num, MAX_DEPTH):
+            able_counts[cur_node] += tree[node][depth] if not alarms[node] else -tree[node][depth]
+
+            if depth > num:
+                tree[cur_node][depth - num] += tree[node][depth] if not alarms[node] else -tree[node][depth]
+            
+        if not alarms[cur_node]:
+            break
         
-    # 1. 자식노드 부터 시작(depth + 1)
-    # 2. setup설정이 꺼져있으면 넘기기
-    # 3. 세기를 확인하고 가능하면 + 1
-    # for child in tree_dict[node]:
-    #     if alarms[child-1]:
-    #         count += can_alarms(child, depth + 1)
+        cur_node = parents[cur_node]
+        num += 1
+    
+    alarms[node] = not alarms[node]
 
-    #         if authorities[child-1] >= depth:
-    #             count += 1
+def set_power(node: int, power: int):
+    before_power = authorities[node]
+    power = min(power, 20)
 
-    return count
+    authorities[node] = power
+
+    tree[node][before_power] -= 1
+    if alarms[node]:
+        cur_node = parents[node]
+        num = 1
+
+        while cur_node:
+            if before_power >= num:
+                able_counts[cur_node] -= 1
+            if before_power > num:
+                tree[cur_node][before_power - num] -= 1
+            if not alarms[cur_node]:
+                break
+            
+            cur_node = parents[cur_node]
+            num += 1
+
+    tree[node][power] += 1
+    if alarms[node]:
+        cur_node = parents[node]
+        num = 1
+
+        while cur_node:
+            if power >= num:
+                able_counts[cur_node] += 1
+            if power > num:
+                tree[cur_node][power - num] += 1
+            if not alarms[cur_node]:
+                break
+
+            cur_node = parents[cur_node]
+            num += 1
+
+def exchange_node(a: int, b: int):
+    before_alarm_a = alarms[a]
+    before_alarm_b = alarms[b]
+
+    if before_alarm_a:
+        set_alarm(a)
+    if before_alarm_b:
+        set_alarm(b)
+
+    parents[a], parents[b] = parents[b], parents[a]
+
+    if before_alarm_a:
+        set_alarm(a)
+    if before_alarm_b:
+        set_alarm(b) 
+
+
+def can_alarms(node: int)-> int:
+    return able_counts[node]
 
 
 if __name__ == '__main__':
+    MAX_NODE = 100001
+    MAX_DEPTH = 21
+
+    parents = [0] * MAX_NODE
+    authorities = [0] * MAX_NODE
+    able_counts = [0] * MAX_NODE
+    alarms = [True] * MAX_NODE
+    tree = [[0 for _ in range(MAX_DEPTH)] for _ in range(MAX_NODE)]
+
     N, Q = map(int, input().split())
-
-    tree = []
-    authorities = []
-    alarms = [True for _ in range(N)]
-
-    tree_dict = dict()
 
     for q in range(Q):
         commands = list(map(int, input().split()))
@@ -50,34 +124,12 @@ if __name__ == '__main__':
         command = commands[0]
 
         if command == 100:
-            tree = commands[1:N+1]
-            authorities = commands[N+1:]
-
-            for i, p in enumerate(tree):
-                if not tree_dict.get(p):
-                    tree_dict[p] = [i+1]
-                else:
-                    tree_dict[p].append(i+1)
-        
+            setup(commands)
         elif command == 200:
-            alarms[commands[1]-1] = not alarms[commands[1]-1]
-        
+            set_alarm(commands[1])
         elif command == 300:
-            authorities[commands[1]-1] = commands[2]
-        
+            set_power(commands[1], commands[2])
         elif command == 400:
-            a = commands[1]
-            b = commands[2]
-
-            temp = tree[a-1]
-            tree[a-1] = tree[b-1]
-            tree[b-1] = temp
-
-            a_index = tree_dict[tree[b-1]].index(a)
-            b_index = tree_dict[tree[a-1]].index(b)
-            
-            tree_dict[tree[a-1]][b_index] = a
-            tree_dict[tree[b-1]][a_index] = b
-
+            exchange_node(commands[1], commands[2])
         elif command == 500:
             print(can_alarms(commands[1]))
